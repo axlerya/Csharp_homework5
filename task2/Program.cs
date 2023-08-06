9fc1d58cfd81257719c723f70e2334be50af6192
axlerya <white_cat_02@mail.ru>
2023-08-06 16:31:30 +0300

    feat: add task 2
    add a solution to the second  task

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] myArray = new int[5]; // Создаем и заполняем массив случайными трехзначными числами
+Random rand = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rand.Next(-10, 10);
+}
+
+int SumOddPosition(int[] arr)
+{
+    int sumOdd = 0;
+    for (int i = 1; i < arr.Length; i+=2) sumOdd+=arr[i];
+    return sumOdd;
+}
+
+Console.Write(string.Join(", ", myArray));
+Console.WriteLine(" ");
+Console.WriteLine(SumOddPosition(myArray));
