2ad285389127d29c755957c8e2a89dfabc533198
axlerya <white_cat_02@mail.ru>
2023-08-06 16:41:59 +0300

    feat: add task 3
    add a solution to the third task

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿double[] myArray = new double[5];
+Random rand = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] =rand.Next(-100,100) + rand.NextDouble();
+}
+
+Console.Write(string.Join(", ", myArray));
+Console.WriteLine(" ");
+Console.WriteLine($"Сумма максимального {myArray.Max()} и минимального значения {myArray.Min()} = " + (myArray.Max() + myArray.Min()));
