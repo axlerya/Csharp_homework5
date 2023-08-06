44b79657be1816c4fc59a922822ce190fc85edb2
axlerya <white_cat_02@mail.ru>
2023-08-06 16:21:31 +0300

    feat: add task 1
    add a solution to the first  task

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] myArray = new int[5]; // Создаем и заполняем массив случайными трехзначными числами
+Random rand = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = rand.Next(100, 1000);
+}
+
+int SumEven(int[] arr) // метод который считает сколько четных чисел в массиве
+{
+    int numEven = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ((arr[i] % 2) == 0)
+        {
+            numEven++;
+        }
+    }
+    return numEven;
+}
+
+Console.Write(string.Join(", ", myArray));
+Console.WriteLine(" ");
+Console.WriteLine(SumEven(myArray));
